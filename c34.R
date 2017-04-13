dfec93a8ea90b2d6aee002f003c5d4945f0ed813
MiguelHernandezMorales <MiguelHernandezMorales@users.noreply.github.com>
2017-04-13 09:56:13 -0300

    Add files via upload

--- c34.R
+++ c34.R
@@ -137,4 +137,75 @@ esc.promedio.ocupados.nuble.sexo. =
 
 write.csv(esc.promedio.ocupados.nuble.sexo., "esc_promedio_ocupados_nuble_sexo.csv")
 
+# Porcentaje de los ocupados con educación superior completa NACIONAL
+tasa.ocupados.profesional.nacional = svyby(~I(cae_general=="Ocupado" & educ>=7),
+                                      by=~sexo, denominator=~I(cae_general=="Ocupado"), 
+                                      info2, svyratio, multicore = TRUE, 
+                                      drop.empty.groups = FALSE, na.rm=TRUE)
+
+freq= xtabs(~I(cae_general=="Ocupado" & educ>=7)+sexo, data=info2$variables)[2,]
+
+tasa.ocupados.profesional.nacional$cv = 
+  tasa.ocupados.profesional.nacional$`se.I(cae_general == \"Ocupado\" & educ >= 7)/I(cae_general == \"Ocupado\")`/tasa.ocupados.profesional.nacional$`I(cae_general == \"Ocupado\" & educ >= 7)/I(cae_general == \"Ocupado\")`
+
+tasa.ocupados.profesional.nacional$frecuencia = freq
+
+names(tasa.ocupados.profesional.nacional) = 
+  c("Jornada", "tasa.mujeres.jornada.nacional", "error estandar","cv","frecuencia")
+
+write.csv(tasa.ocupados.profesional.nacional, "tasa_ocupados_profesional_nacional.csv")
+
+#
+tasa.promedio.ocupados.profesional.nacional = list()
+for (i in 1:length(info)){
+  tasa.promedio.ocupados.profesional.nacional[[i]] = svyratio(~I(cae_general=="Ocupado" & educ>=7), 
+                                                         denominator=~I(cae_general=="Ocupado"), 
+                                                         info[[i]], multicore = TRUE, 
+                                                         drop.empty.groups = FALSE, na.rm=TRUE)
+}
+
+tasa.promedio.ocupados.profesional.nacional. = 
+  do.call(rbind, tasa.promedio.ocupados.profesional.nacional)
+
+names(tasa.promedio.ocupados.profesional.nacional.) = 
+  c("tasa.promedio.ocupados.profesional.nacional", "error estandar")
+
+write.csv(tasa.promedio.ocupados.profesional.nacional., "tasa_promedio_ocupados_profesional_nacional.csv")
+
+
+# Porcentaje de los ocupados con educación superior completa NUBLE
+tasa.ocupados.profesional.nuble = svyby(~I(prov_e==84 & educ>=7),
+                                           by=~sexo, denominator=~I(prov_e==84), 
+                                           info2, svyratio, multicore = TRUE, 
+                                           drop.empty.groups = FALSE, na.rm=TRUE)
+
+freq= xtabs(~I(prov_e==84 & educ>=7)+sexo, data=info2$variables)[2,]
+
+tasa.ocupados.profesional.nuble$cv = 
+  tasa.ocupados.profesional.nuble$`se.I(prov_e == 84 & educ >= 7)/I(prov_e == 84)`/tasa.ocupados.profesional.nuble$`I(prov_e == 84 & educ >= 7)/I(prov_e == 84)`
+
+tasa.ocupados.profesional.nuble$frecuencia = freq
+
+names(tasa.ocupados.profesional.nuble) = 
+  c("sexo", "tasa.ocupados.profesional.nuble", "error estandar","cv","frecuencia")
+
+write.csv(tasa.ocupados.profesional.nuble, "tasa_ocupados_profesional_nuble.csv")
+
+#
+tasa.promedio.ocupados.profesional.nuble = list()
+for (i in 1:length(info)){
+  tasa.promedio.ocupados.profesional.nuble[[i]] = svyratio(~I(prov_e==84 & educ>=7), 
+                                                              denominator=~I(prov_e==84), 
+                                                              info[[i]], multicore = TRUE, 
+                                                              drop.empty.groups = FALSE, na.rm=TRUE)
+}
+
+tasa.promedio.ocupados.profesional.nuble. = 
+  do.call(rbind, tasa.promedio.ocupados.profesional.nuble)
+
+names(tasa.promedio.ocupados.profesional.nuble.) = 
+  c("tasa.promedio.ocupados.profesional.nacional", "error estandar")
+
+write.csv(tasa.promedio.ocupados.profesional.nuble., "tasa_promedio_ocupados_profesional_nuble.csv")
+
 ### FIN CUADRO 34
